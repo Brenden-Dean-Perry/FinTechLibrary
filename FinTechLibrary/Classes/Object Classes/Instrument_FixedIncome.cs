9e6e0371b3be9ec9310a5c0636634395e6f52660
Brenden Perry <brendenperry5@gmail.com>
2023-10-02 20:39:36 -0700

    updates to data structures

--- FinTechLibrary/Classes/Object Classes/Instrument_FixedIncome.cs
+++ FinTechLibrary/Classes/Object Classes/Instrument_FixedIncome.cs
@@ -8,5 +8,12 @@ namespace FinTechLibrary
 {
     internal class Instrument_FixedIncome : FinancialInstrument
     {
+        //Macaulay Duration is a weighted average of the times until the cash flows are recieved
+        internal decimal macaulayDuration { get; set; }
+        //Modified duration is the interest rate sensitivity of a bond's price
+        internal decimal modifiedDuration { get; set; }
+        //Effective duration is similar to modified duration but its cash flows are option adjusted
+        internal decimal effectiveDuration { get; set; }
+        internal decimal convexity { get; set; }
     }
 }
---
+++ FinTechLibrary/Classes/Object Classes/TimeSeries.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinTechLibrary
+{
+    internal class TimeSeries
+    {
+        Dictionary<DateTime, decimal> timeSeries = new Dictionary<DateTime, decimal>();
+
+        public decimal AverageValue()
+        {
+            return timeSeries.Values.Average();
+        }
+
+        public long CountValues()
+        {
+            return timeSeries.Values.LongCount();
+        }
+
+        public decimal TotalPercentChange()
+        {
+            DateTime minDateTime = timeSeries.Keys.Min();
+            DateTime maxDateTime = timeSeries.Keys.Max();
+
+            decimal startingValue = timeSeries.GetValueOrDefault(minDateTime);
+            decimal endingValue = timeSeries.GetValueOrDefault(maxDateTime);
+
+            return (endingValue - startingValue) / startingValue;
+        }
+    }
+}
